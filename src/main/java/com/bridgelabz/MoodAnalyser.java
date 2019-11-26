b17cdea84ed2f98b40fc2340d429a33eb8325676
Upendra Sahu <usahu998@gmail.com>
2019-11-26 11:03:16 +0530

    Refactor 1 code to take mood msg in Constructor

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,11 +1,22 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
-    public String analyseMood(String message) {
+
+    private String message;
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
         if (message.contains("Sad")){
             return "SAD";
         }else{
             return "HAPPY";
         }
     }
+
+
 }
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -6,15 +6,15 @@ import org.junit.Test;
 public class MoodAnalyserTest {
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood =moodAnalyser.analyseMood("I am in Sad Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+        String mood =moodAnalyser.analyseMood();
         Assert.assertEquals("SAD",mood);
     }
 
     @Test
     public void givenMessage_WhenHappy_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood =moodAnalyser.analyseMood("I am in Happy Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
+        String mood =moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY",mood);
     }
 }
